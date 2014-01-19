5ab88582584a222daa092df3c408e598cc010046
Annie Tang <annietang92@gmail.com>
2014-01-19 01:48:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shine::Application.config.secret_key_base = 'bd05b433de229b00b33e2ef660c558706f676223dcd319345ba58a1b576349a96ce8add11523810dd911a42ec8d507ed70365ea91437a2f48a8e3c8d8dc6cdb8'
