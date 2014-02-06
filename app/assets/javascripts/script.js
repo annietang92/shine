82e60ed1e068b47121b5d98804f9f31d4d4de25d
Annie Tang <annietang92@gmail.com>
2014-02-05 20:52:52 -0500

    assets

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -161,39 +161,3 @@ jQuery(document).ready(function ($) {
 	);
 });
 
-/******************
-* = Gallery hover *
-******************/
-jQuery(document).ready(function ($) {
-	//Cache some variables
-	var images = $('#slide-3 a');
-	
-	images.hover(
-		function(e) {
-			var asta = $(this).find('img');
-			$('#slide-3 img').not( asta ).stop(false, false).animate(
-				{
-					opacity: .5
-				},
-				'fast',
-				'linear'
-			);
-			var zoom = $('<div class="zoom"></div>');
-			if ( $(this).hasClass('video') ) {
-				zoom.addClass('video');
-			}
-			$(this).prepend(zoom);
-		},
-		function(e) {
-			$('#slide-3 img').stop(false, false).animate(
-				{
-					opacity: 1
-				},
-				'fast',
-				'linear'
-			);
-			$('.zoom').remove();
-		}
-	);
-});
-
