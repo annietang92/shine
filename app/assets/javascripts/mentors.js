f105acf8c01e84a3fa5d4c9ef35bd27563f46756
Annie Tang <annietang92@gmail.com>
2014-02-07 02:25:52 -0500

    mentors

---
+++ app/assets/javascripts/mentors.js
@@ -0,0 +1,87 @@
+$( document ).ready(function() {
+$( "#kirin_link" ).click(function() {
+  $(".blurb").addClass("hide");
+  $("#kirin").removeClass("hide");
+});
+
+$( "#emily_link" ).click(function() {
+  $(".blurb").addClass("hide");
+  $("#emily").removeClass("hide");
+});
+
+$( "#jennifer_link" ).click(function() {
+  $(".blurb").addClass("hide");
+  $("#jennifer").removeClass("hide");
+});
+
+$( "#christine_link" ).click(function() {
+  $(".blurb").addClass("hide");
+  $("#christine").removeClass("hide");
+});
+
+$( "#jennpan_link" ).click(function() {
+  $(".blurb").addClass("hide");
+  $("#jennpan").removeClass("hide");
+});
+
+$( "#martha_link" ).click(function() {
+  $(".blurb").addClass("hide");
+  $("#martha").removeClass("hide");
+});
+
+$( "#kathy_link" ).click(function() {
+  $(".blurb").addClass("hide");
+  $("#kathy").removeClass("hide");
+});
+
+$( "#bari_link" ).click(function() {
+  $(".blurb").addClass("hide");
+  $("#bari").removeClass("hide");
+});
+
+$( "#mayowa_link" ).click(function() {
+  $(".blurb").addClass("hide");
+  $("#mayowa").removeClass("hide");
+});
+
+$( "#amy_link" ).click(function() {
+  $(".blurb").addClass("hide");
+  $("#amy").removeClass("hide");
+});
+
+$( "#claire_link" ).click(function() {
+  $(".blurb").addClass("hide");
+  $("#claire").removeClass("hide");
+});
+
+$( "#shelby_link" ).click(function() {
+  $(".blurb").addClass("hide");
+  $("#shelby").removeClass("hide");
+});
+
+
+$( "#about_link" ).click(function() {
+  $(".toggle").addClass("hide");
+  $("#about").removeClass("hide");
+});
+
+$( "#program_link" ).click(function() {
+  $(".toggle").addClass("hide");
+  $("#program").removeClass("hide");
+});
+
+$( "#deadlines_link" ).click(function() {
+  $(".toggle").addClass("hide");
+  $("#deadlines").removeClass("hide");
+});
+
+$( "#mentors_link" ).click(function() {
+  $(".toggle").addClass("hide");
+  $("#mentors").removeClass("hide");
+});
+
+$( "#success_link" ).click(function() {
+  $(".toggle").addClass("hide");
+  $("#success").removeClass("hide");
+});
+});
